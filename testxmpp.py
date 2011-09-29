93ae9b00bc18aac6ea5a9746c266fdd0511447f6
Sheyne <me@sheyne.com>
2011-09-28 21:55:29 -0600

    Added hack

---
+++ testxmpp.py
@@ -0,0 +1,22 @@
+import os
+import time
+
+import PyGtalkRobot
+
+
+class SampleBot(PyGtalkRobot.GtalkRobot):
+	pass
+
+
+if __name__ == "__main__":
+	bot = SampleBot()
+	@bot.command
+	def say(message):
+		print "saying message:",message
+		os.popen("say "+repr(message))
+	@bot.command
+	def default(message):
+		print "hit default:",message
+	
+	bot.setState('available', "Active for duty")
+	bot.start("comprehend@sheyne.com", "blah1112")
