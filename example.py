2a9f82d7104dfa37e2852c8199d5fcdd654249b1
Sheyne <me@sheyne.com>
2011-11-27 17:13:48 -0700

    Created a function to control pianobar
    Not actually that useful.

--- example.py
+++ example.py
@@ -35,7 +35,44 @@ def main():
 			for command in self.ui.conversationTypes:
 				self.put("# %s #\n%s" % (command, self.ui.conversationTypes[command].description))
 			self.finish()
-
+	@bot.conversationType
+	class pandora(chatUI.ShellConversation):
+		description = "pandora commands conversation"
+		usage ="""pandora `command'
+## Commands
++    love song
+-    ban song
+a    add music to station
+c    create new station
+d    delete station
+e    explain why this song is played
+g    add genre station
+h    song history
+i    print information about song/station
+j    add shared station
+m    move song to different station
+n    next song
+p    pause/continue
+q    quit
+r    rename station
+s    change station
+t    tired (ban song for 1 month)
+u    upcoming songs
+x    select quickmix stations
+b    bookmark song/artist
+(    decrease volume
+)    increase volume
+=    delete seeds/feedback"""
+		@chatUI.command
+		def default(self,command):
+			if len(command[0]) == 1:
+				with file("/Users/sheyne/.config/pianobar/ctl", "w") as f:
+					f.write(command[0])
+	@bot.conversationType
+	class reply(chatUI.Conversation):
+		def setup(self, message):
+			self.put(message[6:])
+			self.finish()
 	@bot.conversationType
 	class shell(chatUI.ShellConversation):
 		description = "shell commands conversation"
